b8c14139c999ef3b0a31f277a928201de93b93e1
Maheshdh <36495289+Maheshdh@users.noreply.github.com>
2024-03-16 01:21:56 -0400

    Update 938. Range Sum of BST.py

--- BST/938. Range Sum of BST.py
+++ BST/938. Range Sum of BST.py
@@ -1,11 +1,16 @@
 class Solution:
+    # Time Complexity -> O(n)
+    # Space Complexity -> O(log(n))
     def preorder(self,root,ans,low,high):
         if not root:
             return 
+        # since the node's value is less than the lower range, just traverse the right half of the node, as it could contain values within the range
         if(root.val < low):
             self.preorder(root.right,ans,low,high)
+        # similarly traverse only the left half if the node's value is greater than the higher value
         if(root.val > high):
             self.preorder(root.left,ans,low,high)
+        # if the node's value is within bounds, we have a valid node. Hence add the value to the result and traverse left and right half.
         elif(root.val >= low and root.val <= high):
             ans[0] += root.val
             self.preorder(root.left, ans, low, high)
@@ -13,7 +18,7 @@ class Solution:
         
 
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-
+        # set intial result as 0 and traverse tree in preorder fashion.
         ans = [0]
         self.preorder(root,ans,low, high)
         return ans[0]
